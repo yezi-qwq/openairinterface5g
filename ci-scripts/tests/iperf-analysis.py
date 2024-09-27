e9f9718f638dd5b9d938f10816ccd831f2cac196
Jaroslava Fiedlerova <Jaroslava.Fiedlerova@openairinterface.org>
2024-09-27 14:34:52 +0200

    CI: add more unit tests for UDP iperf3 analysis function

--- ci-scripts/tests/iperf-analysis.py
+++ ci-scripts/tests/iperf-analysis.py
@@ -15,29 +15,61 @@ class TestIperfAnalysis(unittest.TestCase):
 	def setUp(self):
 		self.iperf_bitrate_threshold = "99"
 		self.iperf_packetloss_threshold = "0"
-		self.target_bitrate = "1"
 
 	def test_iperf_analyzeV3UDP_ok(self):
 		filename_ok = "tests/log/iperf_udp_test_ok.log"
 		msg_filename_ok = "tests/log/iperf_udp_msg_ok.txt"
+		target_bitrate = "1"
 		with open(msg_filename_ok, 'r') as file:
 			expected_msg_ok = file.read().strip()
-		success, msg = cls_oaicitest.Iperf_analyzeV3UDP(filename_ok, self.iperf_bitrate_threshold, self.iperf_packetloss_threshold, self.target_bitrate)
+		success, msg = cls_oaicitest.Iperf_analyzeV3UDP(filename_ok, self.iperf_bitrate_threshold, self.iperf_packetloss_threshold, target_bitrate)
+		self.assertEqual(msg, expected_msg_ok)
+		self.assertTrue(success)
+
+	def test_iperf_analyzeV3UDP_ok2(self):
+		filename_ok = "tests/log/iperf_udp_test_ok2.log"
+		msg_filename_ok = "tests/log/iperf_udp_msg_ok2.txt"
+		target_bitrate = "4.67"
+		with open(msg_filename_ok, 'r') as file:
+			expected_msg_ok = file.read().strip()
+		success, msg = cls_oaicitest.Iperf_analyzeV3UDP(filename_ok, self.iperf_bitrate_threshold, self.iperf_packetloss_threshold, target_bitrate)
 		self.assertEqual(msg, expected_msg_ok)
 		self.assertTrue(success)
 
 	def test_iperf_analyzeV3UDP_nok(self):
 		filename_nok = "tests/log/iperf_udp_test_nok.log"
 		msg_filename_nok = "tests/log/iperf_udp_msg_nok.txt"
+		target_bitrate = "1"
 		with open(msg_filename_nok, 'r') as file:
 			expected_msg_nok = file.read().strip()
-		success, msg = cls_oaicitest.Iperf_analyzeV3UDP(filename_nok, self.iperf_bitrate_threshold, self.iperf_packetloss_threshold, self.target_bitrate)
+		success, msg = cls_oaicitest.Iperf_analyzeV3UDP(filename_nok, self.iperf_bitrate_threshold, self.iperf_packetloss_threshold, target_bitrate)
+		self.assertEqual(msg, expected_msg_nok)
+		self.assertFalse(success)
+
+	def test_iperf_analyzeV3UDP_nok2(self):
+		filename_nok = "tests/log/iperf_udp_test_nok2.log"
+		msg_filename_nok = "tests/log/iperf_udp_msg_nok2.txt"
+		target_bitrate = "80"
+		with open(msg_filename_nok, 'r') as file:
+			expected_msg_nok = file.read().strip()
+		success, msg = cls_oaicitest.Iperf_analyzeV3UDP(filename_nok, self.iperf_bitrate_threshold, self.iperf_packetloss_threshold, target_bitrate)
+		self.assertEqual(msg, expected_msg_nok)
+		self.assertFalse(success)
+
+	def test_iperf_analyzeV3UDP_nok3(self):
+		filename_nok = "tests/log/iperf_udp_test_nok3.log"
+		msg_filename_nok = "tests/log/iperf_udp_msg_nok3.txt"
+		target_bitrate = "4.67"
+		with open(msg_filename_nok, 'r') as file:
+			expected_msg_nok = file.read().strip()
+		success, msg = cls_oaicitest.Iperf_analyzeV3UDP(filename_nok, self.iperf_bitrate_threshold, self.iperf_packetloss_threshold, target_bitrate)
 		self.assertEqual(msg, expected_msg_nok)
 		self.assertFalse(success)
 
 	def test_iperf_analyzeV3UDP_notfound(self):
 		filename_notfound = "tests/log/iperf_udp_test_notfound.log"
-		success, msg = cls_oaicitest.Iperf_analyzeV3UDP(filename_notfound, self.iperf_bitrate_threshold, self.iperf_packetloss_threshold, self.target_bitrate)
+		target_bitrate = "1"
+		success, msg = cls_oaicitest.Iperf_analyzeV3UDP(filename_notfound, self.iperf_bitrate_threshold, self.iperf_packetloss_threshold, target_bitrate)
 		self.assertEqual(msg, "Iperf3 UDP: Log file not present")
 		self.assertFalse(success)
 
