a343ec03fd7e929368b273d60f91917ca196e070
francescomani <email@francescomani.it>
2024-10-10 11:58:07 +0200

    changes in get_transformPrecoding to follow what's written in 6.1.3 of 214

--- openair2/LAYER2/NR_MAC_COMMON/nr_mac_common.c
+++ openair2/LAYER2/NR_MAC_COMMON/nr_mac_common.c
@@ -2658,16 +2658,17 @@ long get_transformPrecoding(const NR_UE_UL_BWP_t *current_UL_BWP, nr_dci_format_
       && current_UL_BWP->configuredGrantConfig->transformPrecoder)
     return *current_UL_BWP->configuredGrantConfig->transformPrecoder;
 
-  if (dci_format == NR_UL_DCI_FORMAT_0_1
+  long msg3_tp = NR_PUSCH_Config__transformPrecoder_disabled;
+  if (current_UL_BWP && current_UL_BWP->rach_ConfigCommon && current_UL_BWP->rach_ConfigCommon->msg3_transformPrecoder)
+    msg3_tp = NR_PUSCH_Config__transformPrecoder_enabled;
+
+  if (dci_format != NR_UL_DCI_FORMAT_0_0
       && current_UL_BWP
       && current_UL_BWP->pusch_Config
       && current_UL_BWP->pusch_Config->transformPrecoder)
     return *current_UL_BWP->pusch_Config->transformPrecoder;
 
-  if (current_UL_BWP && current_UL_BWP->rach_ConfigCommon && current_UL_BWP->rach_ConfigCommon->msg3_transformPrecoder)
-    return NR_PUSCH_Config__transformPrecoder_enabled;
-
-  return NR_PUSCH_Config__transformPrecoder_disabled;
+  return msg3_tp;
 }
 
 uint8_t get_pusch_nb_antenna_ports(NR_PUSCH_Config_t *pusch_Config,
