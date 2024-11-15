df9aec167e05e75af28ea5f8417e227e6ceec272
Raphael Defosseux <raphael.defosseux@eurecom.fr>
2024-11-15 09:56:35 +0100

    CI: Allow ranCommitID to be "develop"
    This will be used to pull images with the tag "develop" (i.e., without a
    commit ID).

--- ci-scripts/cls_containerize.py
+++ ci-scripts/cls_containerize.py
@@ -70,6 +70,8 @@ def CreateWorkspace(host, sourcePath, ranRepository, ranCommitID, ranTargetBranc
 	return ret.returncode == 0
 
 def CreateTag(ranCommitID, ranBranch, ranAllowMerge):
+	if ranCommitID == 'develop':
+		return 'develop'
 	shortCommit = ranCommitID[0:8]
 	if ranAllowMerge:
 		# Allowing contributor to have a name/branchName format
