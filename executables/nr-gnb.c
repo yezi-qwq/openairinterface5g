c5a76d66542632b8cf0b1620209fe00ef6e6bb3e
Robert Schmidt <robert.schmidt@openairinterface.org>
2024-12-06 17:37:02 +0100

    Remove unnecessary handle_nr_slot_ind()
    the slot indication is sent from the tx_thread (tx_func()). Thus, we
    don't need to send it a second time.

--- executables/nr-gnb.c
+++ executables/nr-gnb.c
@@ -197,15 +197,6 @@ static void rx_func(processingData_L1_t *info)
   int rt_prof_idx = absslot_rx % RT_PROF_DEPTH;
   clock_gettime(CLOCK_MONOTONIC, &info->gNB->rt_L1_profiling.start_L1_RX[rt_prof_idx]);
 
-  // *******************************************************************
-
-  if (NFAPI_MODE == NFAPI_MODE_PNF) {
-    // I am a PNF and I need to let nFAPI know that we have a (sub)frame tick
-    // LOG_D(PHY, "oai_nfapi_slot_ind(frame:%u, slot:%d) ********\n", frame_rx, slot_rx);
-    handle_nr_slot_ind(frame_rx, slot_rx);
-  }
-  // ****************************************
-
   // RX processing
   int rx_slot_type = nr_slot_select(cfg, frame_rx, slot_rx);
   if (rx_slot_type == NR_UPLINK_SLOT || rx_slot_type == NR_MIXED_SLOT) {
