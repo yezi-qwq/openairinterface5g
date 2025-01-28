saffanazyan07/oai_w-agf

Use is_mixed_slot in stack

openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
@@ -974,7 +974,7 @@ static bool get_feasible_msg3_tda(const NR_ServingCellConfigCommon_t *scc,
 
     const tdd_bitmap_t *tdd_slot_bitmap = tdd_config->period_cfg.tdd_slot_bitmap;
     // check if enough symbols in case of mixed slot
-    bool is_mixed = (tdd_slot_bitmap[temp_slot % tdd_period_slot].slot_type == TDD_NR_MIXED_SLOT);
+    bool is_mixed = is_mixed_slot(temp_slot, tdd_config);
     // if the mixed slot has not enough symbols, skip
     if (is_mixed && tdd_slot_bitmap[temp_slot % tdd_period_slot].num_ul_symbols < 3)
       continue;

yezi-qwq/openairinterface5g

openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
@@ -982,7 +982,7 @@ static bool get_feasible_msg3_tda(const NR_ServingCellConfigCommon_t *scc,
     const tdd_bitmap_t *tdd_slot_bitmap = fs->period_cfg.tdd_slot_bitmap;
     int s = get_slot_idx_in_period(temp_slot, fs);
     // check if enough symbols in case of mixed slot
-    bool is_mixed = (tdd_slot_bitmap[s].slot_type == TDD_NR_MIXED_SLOT);
+    bool is_mixed = is_mixed_slot(s, fs);
     // if the mixed slot has not enough symbols, skip
     if (is_mixed && tdd_slot_bitmap[s].num_ul_symbols < 3)
       continue;