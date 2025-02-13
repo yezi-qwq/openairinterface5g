2f6b80b44450d0da9022ee4d228d01b44df6983c
francescomani <email@francescomani.it>
2025-02-13 14:37:18 +0100

    remove unnecessary Type0-PDCCH externs

--- openair2/LAYER2/NR_MAC_COMMON/nr_mac_extern.h
+++ openair2/LAYER2/NR_MAC_COMMON/nr_mac_extern.h
@@ -44,68 +44,6 @@ extern uint8_t nfapi_mode;
 extern const uint32_t NR_SHORT_BSR_TABLE[NR_SHORT_BSR_TABLE_SIZE];
 extern const uint32_t NR_LONG_BSR_TABLE[NR_LONG_BSR_TABLE_SIZE];
 
-//	Type0-PDCCH search space
-extern const int32_t table_38213_13_1_c1[16];
-extern const int32_t table_38213_13_1_c2[16];
-extern const int32_t table_38213_13_1_c3[16];
-extern const int32_t table_38213_13_1_c4[16];
-
-extern const int32_t table_38213_13_2_c1[16];
-extern const int32_t table_38213_13_2_c2[16];
-extern const int32_t table_38213_13_2_c3[16];
-extern const int32_t table_38213_13_2_c4[16];
-
-extern const int32_t table_38213_13_3_c1[16];
-extern const int32_t table_38213_13_3_c2[16];
-extern const int32_t table_38213_13_3_c3[16];
-extern const int32_t table_38213_13_3_c4[16];
-
-extern const int32_t table_38213_13_4_c1[16];
-extern const int32_t table_38213_13_4_c2[16];
-extern const int32_t table_38213_13_4_c3[16];
-extern const int32_t table_38213_13_4_c4[16];
-
-extern const int32_t table_38213_13_5_c1[16];
-extern const int32_t table_38213_13_5_c2[16];
-extern const int32_t table_38213_13_5_c3[16];
-extern const int32_t table_38213_13_5_c4[16];
-
-extern const int32_t table_38213_13_6_c1[16];
-extern const int32_t table_38213_13_6_c2[16];
-extern const int32_t table_38213_13_6_c3[16];
-extern const int32_t table_38213_13_6_c4[16];
-
-extern const int32_t table_38213_13_7_c1[16];
-extern const int32_t table_38213_13_7_c2[16];
-extern const int32_t table_38213_13_7_c3[16];
-extern const int32_t table_38213_13_7_c4[16];
-
-extern const int32_t table_38213_13_8_c1[16];
-extern const int32_t table_38213_13_8_c2[16];
-extern const int32_t table_38213_13_8_c3[16];
-extern const int32_t table_38213_13_8_c4[16];
-
-extern const int32_t table_38213_13_9_c1[16];
-extern const int32_t table_38213_13_9_c2[16];
-extern const int32_t table_38213_13_9_c3[16];
-extern const int32_t table_38213_13_9_c4[16];
-
-extern const int32_t table_38213_13_10_c1[16];
-extern const int32_t table_38213_13_10_c2[16];
-extern const int32_t table_38213_13_10_c3[16];
-extern const int32_t table_38213_13_10_c4[16];
-
-extern const float   table_38213_13_11_c1[16];
-extern const int32_t table_38213_13_11_c2[16];
-extern const float   table_38213_13_11_c3[16];
-extern const int32_t table_38213_13_11_c4[16];
-
-extern const float   table_38213_13_12_c1[16];
-extern const int32_t table_38213_13_12_c2[16];
-extern const float   table_38213_13_12_c3[16];
-
-extern const int32_t table_38213_10_1_1_c2[5];
-
 extern const char table_38211_6_3_1_5_1[6][2][1];
 extern const char table_38211_6_3_1_5_2[28][4][1];
 extern const char table_38211_6_3_1_5_3[28][4][1];
