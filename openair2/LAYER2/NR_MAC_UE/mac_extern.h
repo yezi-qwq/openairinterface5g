cf9b19a8550bc379360d10bda572267aa2158bef
francescomani <email@francescomani.it>
2025-02-13 15:18:56 +0100

    remove unused tables from UE mac_extern.h

--- openair2/LAYER2/NR_MAC_UE/mac_extern.h
+++ openair2/LAYER2/NR_MAC_UE/mac_extern.h
@@ -32,16 +32,6 @@
 
 #include "mac_defs.h"
 
-//extern NR_UE_MAC_INST_t *UE_mac_inst;
-
-//  DCI extraction
-//  for PUSCH from TS 38.214 subclause 6.1.2.1.1
-extern uint8_t table_6_1_2_1_1_2_time_dom_res_alloc_A[16][3];
-//  for PDSCH from TS 38.214 subclause 5.1.2.1.1
-extern uint8_t table_5_1_2_1_1_2_time_dom_res_alloc_A[16][3];
-
-extern int64_t table_6_3_3_2_3_prachConfig_Index [256][9];
-
 // DCI
 extern const uint8_t table_7_3_1_1_2_2_3_4_5[64][20];
 extern const uint8_t table_7_3_1_1_2_12[14][3];
