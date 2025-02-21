yezi-qwq/openairinterface5g

commit f278b008375d415ad4b6a6d381647581d9afe434

Add encoding/decoding library for F1AP Setup Request (Interface Management)

openair2/F1AP/lib/f1ap_lib_common.c
@@ -23,7 +23,7 @@
 #include "f1ap_messages_types.h"
 
 #include "OCTET_STRING.h"
-
+#include "common/utils/utils.h"
 #include "common/utils/assertions.h"
 #include "common/utils/utils.h"
 
@@ -32,6 +32,79 @@ bool eq_f1ap_plmn(const f1ap_plmn_t *a, const f1ap_plmn_t *b)
   _F1_EQ_CHECK_INT(a->mcc, b->mcc);
   _F1_EQ_CHECK_INT(a->mnc, b->mnc);
   _F1_EQ_CHECK_INT(a->mnc_digit_length, b->mnc_digit_length);
+  return true;
+}
+
+bool eq_f1ap_freq_info(const f1ap_nr_frequency_info_t *a, const f1ap_nr_frequency_info_t *b)
+{
+  if (a->arfcn != b->arfcn)
+    return false;
+  if (a->band != b->band)
+    return false;
+  return true;
+}
+
+bool eq_f1ap_tx_bandwidth(const f1ap_transmission_bandwidth_t *a, const f1ap_transmission_bandwidth_t *b)
+{
+  if (a->nrb != b->nrb)
+    return false;
+  if (a->scs != b->scs)
+    return false;
+  return true;
+}
+
+bool eq_f1ap_cell_info(const f1ap_served_cell_info_t *a, const f1ap_served_cell_info_t *b)
+{
+  if (a->nr_cellid != b->nr_cellid)
+    return false;
+  if (a->nr_pci != b->nr_pci)
+    return false;
+  if (*a->tac != *b->tac)
+    return false;
+  if (a->mode != b->mode)
+    return false;
+  if (a->mode == F1AP_MODE_TDD) {
+    /* TDD */
+    if (!eq_f1ap_tx_bandwidth(&a->tdd.tbw, &b->tdd.tbw))
+      return false;
+    if (!eq_f1ap_freq_info(&a->tdd.freqinfo, &b->tdd.freqinfo))
+      return false;
+  } else if (a->mode == F1AP_MODE_FDD) {
+    /* FDD */
+    if (!eq_f1ap_tx_bandwidth(&a->fdd.dl_tbw, &b->fdd.dl_tbw))
+      return false;
+    if (!eq_f1ap_freq_info(&a->fdd.dl_freqinfo, &b->fdd.dl_freqinfo))
+      return false;
+    if (!eq_f1ap_tx_bandwidth(&a->fdd.ul_tbw, &b->fdd.ul_tbw))
+      return false;
+    if (!eq_f1ap_freq_info(&a->fdd.ul_freqinfo, &b->fdd.ul_freqinfo))
+      return false;
+  }
+  if (a->measurement_timing_config_len != b->measurement_timing_config_len)
+    return false;
+  if (*a->measurement_timing_config != *b->measurement_timing_config)
+    return false;
+  if (!eq_f1ap_plmn(&a->plmn, &b->plmn))
+    return false;
+  return true;
+}
+
+bool eq_f1ap_sys_info(const f1ap_gnb_du_system_info_t *a, const f1ap_gnb_du_system_info_t *b)
+{
+  /* MIB */
+  if (a->mib_length != b->mib_length)
+    return false;
+  for (int i = 0; i < a->mib_length; i++) {
+      if (a->mib[i] != b->mib[i])
+          return false;
+  }
+  /* SIB1 */
+  if (a->sib1_length != b->sib1_length)
+    return false;
+  for (int i = 0; i < a->sib1_length; i++) {
+      if (a->sib1[i] != b->sib1[i])
+          return false;
+  }
   return true;
 }
 

openair2/COMMON/f1ap_messages_types.h
@@ -190,7 +190,7 @@ typedef struct served_cells_to_activate_s {
   f1ap_plmn_t plmn;
   // NR Global Cell Id
   uint64_t nr_cellid;
-  /// NRPCI
+  /// NRPCI [int 0..1007]
   uint16_t nrpci;
   /// num SI messages per DU cell
   uint8_t num_SI;

openair2/F1AP/f1ap_cu_interface_management.c
@@ -34,6 +34,8 @@
 #include "f1ap_encoder.h"
 #include "f1ap_itti_messaging.h"
 #include "f1ap_cu_interface_management.h"
+#include "f1ap_default_values.h"
+#include "lib/f1ap_interface_management.h"
 
 int CU_handle_RESET_ACKNOWLEDGE(instance_t instance, sctp_assoc_t assoc_id, uint32_t stream, F1AP_F1AP_PDU_t *pdu)
 {

openair2/F1AP/lib/CMakeLists.txt
@@ -1,6 +1,7 @@
 add_library(f1ap_lib OBJECT
             f1ap_lib_common.c
             f1ap_rrc_message_transfer.c
+            f1ap_interface_management.c
 )
 target_link_libraries(f1ap_lib PRIVATE asn1_f1ap)
 target_include_directories(f1ap_lib PUBLIC

openair2/F1AP/lib/f1ap_interface_management.h
@@ -19,23 +19,18 @@
  *      contact@openairinterface.org
  */
 
-#ifndef F1AP_LIB_INCLUDES_H_
-#define F1AP_LIB_INCLUDES_H_
+#ifndef F1AP_INTERFACE_MANAGEMENT_H_
+#define F1AP_INTERFACE_MANAGEMENT_H_
 
-#include "F1AP_F1AP-PDU.h"
-#include "F1AP_InitialULRRCMessageTransfer.h"
-#include "F1AP_InitiatingMessage.h"
-#include "F1AP_NRCellIdentity.h"
-#include "F1AP_ProtocolIE-Field.h"
-#include "F1AP_ServedPLMNs-Item.h"
-#include "F1AP_ProtocolExtensionContainer.h"
-#include "F1AP_ProtocolExtensionField.h"
-#include "F1AP_ProtocolIE-Field.h"
-#include "F1AP_SliceSupportItem.h"
-#include "F1AP_NR-Mode-Info.h"
-#include "F1AP_GNB-DU-System-Information.h"
-#include "F1AP_FDD-Info.h"
-#include "F1AP_TDD-Info.h"
-#include "F1AP_FreqBandNrItem.h"
+#include <stdbool.h>
+#include "f1ap_messages_types.h"
 
-#endif /* F1AP_LIB_INCLUDES_H_ */
+struct F1AP_F1AP_PDU;
+
+struct F1AP_F1AP_PDU *encode_f1ap_setup_request(const f1ap_setup_req_t *msg);
+bool decode_f1ap_setup_request(const struct F1AP_F1AP_PDU *pdu, f1ap_setup_req_t *out);
+f1ap_setup_req_t cp_f1ap_setup_request(const f1ap_setup_req_t *msg);
+bool eq_f1ap_setup_request(const f1ap_setup_req_t *a, const f1ap_setup_req_t *b);
+void free_f1ap_setup_request(const f1ap_setup_req_t *msg);
+
+#endif /* F1AP_INTERFACE_MANAGEMENT_H_ */

openair2/F1AP/lib/f1ap_lib_common.h
@@ -25,6 +25,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include "openair3/UTILS/conversions.h"
 
 #ifdef ENABLE_TESTS
   #define PRINT_ERROR(...) fprintf(stderr, ##__VA_ARGS__)
@@ -69,6 +70,14 @@
 
 struct f1ap_plmn_t;
 bool eq_f1ap_plmn(const struct f1ap_plmn_t *a, const struct f1ap_plmn_t *b);
+struct f1ap_served_cell_info_t;
+bool eq_f1ap_cell_info(const struct f1ap_served_cell_info_t *a, const struct f1ap_served_cell_info_t *b);
+struct f1ap_gnb_du_system_info_t;
+bool eq_f1ap_sys_info(const struct f1ap_gnb_du_system_info_t *a, const struct f1ap_gnb_du_system_info_t *b);
+struct f1ap_nr_frequency_info_t;
+bool eq_f1ap_freq_info(const struct f1ap_nr_frequency_info_t *a, const struct f1ap_nr_frequency_info_t *b);
+struct f1ap_transmission_bandwidth_t;
+bool eq_f1ap_tx_bandwidth(const struct f1ap_transmission_bandwidth_t *a, const struct f1ap_transmission_bandwidth_t *b);
 
 struct OCTET_STRING;
 uint8_t *cp_octet_string(const struct OCTET_STRING *os, int *len);

commit 2cc3f3bcac432d185a28f7cb51949c81f2f4b65d

Fix: write NSSAI info of F1AP Cell Info struct
During refactoring, the previously implemented NSSAI field of the cell
info structure was not written to F1AP ASN.1 structures anymore;
eq_f1ap_cell_info() also conveniently omitted the corresponding equality
check. Add this back so the CU can correctly receive the NSSAI in case
of F1 split operation.

Fixes: f278b008375d ("Add encoding/decoding library for F1AP Setup
  Request (Interface Management) ")

openair2/F1AP/lib/f1ap_interface_management.c
@@ -404,6 +404,31 @@ static int read_slice_info(const F1AP_ServedPLMNs_Item_t *plmn, nssai_t *nssai, 
   return ssl->list.count;
 }
 
+static F1AP_ProtocolExtensionContainer_10696P34_t *write_slice_info(int num_ssi, const nssai_t *nssai)
+{
+  if (num_ssi == 0)
+    return NULL;
+
+  F1AP_ProtocolExtensionContainer_10696P34_t *p = calloc_or_fail(1, sizeof(*p));
+  asn1cSequenceAdd(p->list, F1AP_ServedPLMNs_ItemExtIEs_t, served_plmns_itemExtIEs);
+  served_plmns_itemExtIEs->criticality = F1AP_Criticality_ignore;
+  served_plmns_itemExtIEs->id = F1AP_ProtocolIE_ID_id_TAISliceSupportList;
+  served_plmns_itemExtIEs->extensionValue.present = F1AP_ServedPLMNs_ItemExtIEs__extensionValue_PR_SliceSupportList;
+  F1AP_SliceSupportList_t *slice_support_list = &served_plmns_itemExtIEs->extensionValue.choice.SliceSupportList;
+
+  for (int s = 0; s < num_ssi; s++) {
+    asn1cSequenceAdd(slice_support_list->list, F1AP_SliceSupportItem_t, slice);
+    const nssai_t *n = &nssai[s];
+    INT8_TO_OCTET_STRING(n->sst, &slice->sNSSAI.sST);
+    if (n->sd != 0xffffff) {
+      asn1cCalloc(slice->sNSSAI.sD, tmp);
+      INT24_TO_OCTET_STRING(n->sd, tmp);
+    }
+  }
+
+  return p;
+}
+
 /**
  * @brief F1AP Setup Request memory management
  */
@@ -468,6 +493,8 @@ static F1AP_Served_Cell_Information_t encode_served_cell_info(const f1ap_served_
   asn1cSequenceAdd(scell_info.servedPLMNs.list, F1AP_ServedPLMNs_Item_t, servedPLMN_item);
   // PLMN Identity (M)
   MCC_MNC_TO_PLMNID(c->plmn.mcc, c->plmn.mnc, c->plmn.mnc_digit_length, &servedPLMN_item->pLMN_Identity);
+  // NSSAIs (O)
+  servedPLMN_item->iE_Extensions = (struct F1AP_ProtocolExtensionContainer *)write_slice_info(c->num_ssi, c->nssai);
   // NR-Mode-Info (M)
   F1AP_NR_Mode_Info_t *nR_Mode_Info = &scell_info.nR_Mode_Info;
   if (c->mode == F1AP_MODE_FDD) { // FDD Info

openair2/F1AP/lib/f1ap_lib_common.c
@@ -57,6 +57,11 @@ bool eq_f1ap_cell_info(const f1ap_served_cell_info_t *a, const f1ap_served_cell_
     return false;
   if (a->tac)
     _F1_EQ_CHECK_INT(*a->tac, *b->tac);
+  _F1_EQ_CHECK_INT(a->num_ssi, b->num_ssi);
+  for (int i = 0; i < a->num_ssi; ++i) {
+    _F1_EQ_CHECK_INT(a->nssai[i].sst, b->nssai[i].sst);
+    _F1_EQ_CHECK_INT(a->nssai[i].sd, b->nssai[i].sd);
+  }
   _F1_EQ_CHECK_INT(a->mode, b->mode);
   if (a->mode == F1AP_MODE_TDD) {
     /* TDD */