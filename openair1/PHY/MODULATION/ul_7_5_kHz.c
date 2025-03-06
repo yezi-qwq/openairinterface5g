bb93b066634c90315e115601356d673dce63a1dc
Laurent THOMAS <laurent.thomas@open-cells.com>
2025-03-06 10:22:31 +0100

    use one call to multiply the full size vector

--- openair1/PHY/MODULATION/ul_7_5_kHz.c
+++ openair1/PHY/MODULATION/ul_7_5_kHz.c
@@ -27,42 +27,40 @@
 void remove_7_5_kHz(RU_t *ru,uint8_t slot)
 {
 
-  int32_t **rxdata=ru->common.rxdata;
-  int32_t **rxdata_7_5kHz=ru->common.rxdata_7_5kHz;
-  uint32_t *kHz7_5ptr;
-  
-  simde__m128i *rxptr128, *rxptr128_7_5kHz, *kHz7_5ptr128;
+  c16_t **rxdata=(c16_t **)ru->common.rxdata;
+  c16_t **rxdata_7_5kHz=(c16_t **)ru->common.rxdata_7_5kHz;
+  int16_t *kHz7_5;
 
   LTE_DL_FRAME_PARMS *frame_parms=ru->frame_parms;
 
   switch (frame_parms->N_RB_UL) {
 
   case 6:
-    kHz7_5ptr = (frame_parms->Ncp==0) ? (uint32_t*)s6n_kHz_7_5 : (uint32_t*)s6e_kHz_7_5;
+    kHz7_5 = (frame_parms->Ncp==0) ? s6n_kHz_7_5 : s6e_kHz_7_5;
     break;
 
   case 15:
-    kHz7_5ptr = (frame_parms->Ncp==0) ? (uint32_t*)s15n_kHz_7_5 : (uint32_t*)s15e_kHz_7_5;
+    kHz7_5 = (frame_parms->Ncp==0) ? s15n_kHz_7_5 : s15e_kHz_7_5;
     break;
 
   case 25:
-    kHz7_5ptr = (frame_parms->Ncp==0) ? (uint32_t*)s25n_kHz_7_5 : (uint32_t*)s25e_kHz_7_5;
+    kHz7_5 = (frame_parms->Ncp==0) ? s25n_kHz_7_5 : s25e_kHz_7_5;
     break;
 
   case 50:
-    kHz7_5ptr = (frame_parms->Ncp==0) ? (uint32_t*)s50n_kHz_7_5 : (uint32_t*)s50e_kHz_7_5;
+    kHz7_5 = (frame_parms->Ncp==0) ? s50n_kHz_7_5 : s50e_kHz_7_5;
     break;
 
   case 75:
-    kHz7_5ptr = (frame_parms->Ncp==0) ? (uint32_t*)s75n_kHz_7_5 : (uint32_t*)s75e_kHz_7_5;
+    kHz7_5 = (frame_parms->Ncp==0) ? s75n_kHz_7_5 : s75e_kHz_7_5;
     break;
 
   case 100:
-    kHz7_5ptr = (frame_parms->Ncp==0) ? (uint32_t*)s100n_kHz_7_5 : (uint32_t*)s100e_kHz_7_5;
+    kHz7_5 = (frame_parms->Ncp==0) ? s100n_kHz_7_5 : s100e_kHz_7_5;
     break;
 
   default:
-    kHz7_5ptr = (frame_parms->Ncp==0) ? (uint32_t*)s25n_kHz_7_5 : (uint32_t*)s25e_kHz_7_5;
+    kHz7_5 = (frame_parms->Ncp==0) ? s25n_kHz_7_5 : s25e_kHz_7_5;
     break;
   }
 
@@ -72,25 +70,16 @@ void remove_7_5_kHz(RU_t *ru,uint8_t slot)
   uint16_t len = frame_parms->samples_per_tti/2;
 
   for (uint8_t aa=0; aa<ru->nb_rx; aa++) {
-    rxptr128 = (simde__m128i *)&rxdata[aa][slot_offset];
-    rxptr128_7_5kHz = (simde__m128i *)&rxdata_7_5kHz[aa][slot_offset2];
-    kHz7_5ptr128 = (simde__m128i *)kHz7_5ptr;
+    c16_t *rxptr = rxdata[aa]+slot_offset;
+    c16_t *rxptr_7_5kHz = rxdata_7_5kHz[aa]+slot_offset2;
     // apply 7.5 kHz
-
-    //      if (((slot>>1)&1) == 0) { // apply the sinusoid from the table directly
-    for (uint32_t i=0; i<(len>>2); i++) {
-      rxptr128_7_5kHz[i] = oai_mm_cpx_mult(kHz7_5ptr128[i], rxptr128[i], 15);
-    }
+    mult_complex_vectors((c16_t*)kHz7_5,rxptr,rxptr_7_5kHz,len, 15);
     // undo 7.5 kHz offset for symbol 3 in case RU is slave (for OTA synchronization)
     if (ru->is_slave == 1 && slot == 2){
-      memcpy((void*)&rxdata_7_5kHz[aa][(3*frame_parms->ofdm_symbol_size)+
-                                (2*frame_parms->nb_prefix_samples)+
-                                frame_parms->nb_prefix_samples0],
-             (void*)&rxdata[aa][slot_offset+ru->N_TA_offset+
-                           (3*frame_parms->ofdm_symbol_size)+
-                           (2*frame_parms->nb_prefix_samples)+
-                           frame_parms->nb_prefix_samples0],
-             (frame_parms->ofdm_symbol_size + frame_parms->nb_prefix_samples)*sizeof(int32_t));
+      int offset=3*frame_parms->ofdm_symbol_size+2*frame_parms->nb_prefix_samples+frame_parms->nb_prefix_samples0;
+      memcpy(rxdata_7_5kHz[aa]+offset,
+             rxdata[aa]+offset,
+             (frame_parms->ofdm_symbol_size + frame_parms->nb_prefix_samples)*sizeof(c16_t));
     }
   }
 }
