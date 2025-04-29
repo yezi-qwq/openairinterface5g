dfa1bd4efe7c3c58661f52d684cb3c0f94932987
Teodora <teodora.vladic@openairinterface.org>
2025-04-29 17:55:26 +0200

    Add the NSSAI logging condition in KPM
    It is a periodic output, based on the reporting period.

--- openair2/E2AP/RAN_FUNCTION/O-RAN/ran_func_kpm.c
+++ openair2/E2AP/RAN_FUNCTION/O-RAN/ran_func_kpm.c
@@ -173,6 +173,7 @@ static void capture_sst_sd(test_cond_value_t* test_cond_value, uint8_t *sst, uin
         *sd = malloc(**sd);
         **sd = buf[1] << 16 | buf[2] << 8 | buf[3];
       }
+      printf("[E2 AGENT][E2SM-KPM] Condition NSSAI (%d, %x).\n", *sst, (*sd) ? **sd : 0xffffff);
       break;
     }
     default:
@@ -408,7 +409,10 @@ bool read_kpm_sm(void* data)
         ue_type_matcher fp_match_cond_type = (*match_cond_arr[test_info.test_cond_type])[node_type];
         arr_ue_id_t arr_ue_id = fp_match_cond_type(test_info);
 
-        if (arr_ue_id.sz == 0) return false;
+        if (arr_ue_id.sz == 0) {
+          printf("[E2 AGENT][E2SM-KPM] No UE matches the condition criteria.\n");
+          return false;
+        }
         kpm->ind.msg.frm_3 = fill_kpm_ind_msg_frm_3(&arr_ue_id, &frm_4->action_def_format_1);
       }
       break;
