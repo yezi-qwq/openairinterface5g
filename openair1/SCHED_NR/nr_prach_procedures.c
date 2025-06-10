964c737b7d3ad6b9249a3322c4cd2190f9b610c6
francescomani <email@francescomani.it>
2025-06-10 11:12:44 +0200

    simplify get_nr_prach_duration function

--- openair1/SCHED_NR/nr_prach_procedures.c
+++ openair1/SCHED_NR/nr_prach_procedures.c
@@ -37,63 +37,14 @@
 #include "nfapi_pnf.h"
 #include "common/utils/LOG/log.h"
 #include "common/utils/LOG/vcd_signal_dumper.h"
-
-
 #include "assertions.h"
-
 #include <time.h>
 
 int get_nr_prach_duration(uint8_t prach_format)
 {
-  switch(prach_format){
-
-      case 0:  // format 0
-         return 0;
-
-      case 1:  // format 1
-         return 0;
-
-      case 2:  // format 2
-         return 0;
-
-      case 3:  // format 3
-         return 0;
-
-      case 4:  // format A1
-         return 2;
-
-      case 5:  // format A2
-         return 4;
-
-      case 6:  // format A3
-         return 6;
-
-      case 7:  // format B1
-         return 2;
-
-      case 8:  // format B4
-         return 12;
-
-      case 9:  // format C0
-         return 2;
-
-      case 10:  // format C2
-         return 6;
-
-      case 11:  // format A1/B1
-         return 2;
-
-      case 12:  // format A2/B2
-         return 4;
-
-      case 13:  // format A3/B3
-         return 6;
-
-      default :
-         AssertFatal(1==0,"Invalid Prach format\n");
-         break;
-
-  }
+  const int val[14] = {0, 0, 0, 0, 2, 4, 6, 2, 12, 2, 6, 2, 4, 6};
+  AssertFatal(prach_format < sizeofArray(val), "Invalid Prach format %d\n", prach_format);
+  return val[prach_format];
 }
 
 void L1_nr_prach_procedures(PHY_VARS_gNB *gNB, int frame, int slot, nfapi_nr_rach_indication_t *rach_ind)
